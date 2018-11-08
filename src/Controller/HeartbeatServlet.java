c5f0e133c63737fde94281b16f0f31fbd19cd071
白杨 <baiyangwd@163.com>
2018-11-08 18:33:52 +0800

    serlvet路径写错了

--- src/Controller/HeartbeatServlet.java
+++ src/Controller/HeartbeatServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "HeartbeatServlet")
+@WebServlet("/HeartbeatServlet")
 public class HeartbeatServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         doGet(request, response);
